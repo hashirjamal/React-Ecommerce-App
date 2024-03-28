90fc176299fdc4175a7b6a3ec86f7bc63ca6c245
Hashir <hashirjamal18@gmail.com>
2024-03-28 20:10:35 +0500

    INITIALIZING REP

---
+++ src/home/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MenuItem from './MenuItem'
+import "./homepage.scss"
+export default function Homepage() {
+  return (
+    <div className='home-container'>
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+    </div>
+  )
+}
---
+++ src/home/MenuItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./menu-item.scss"
+export default function MenuItem() {
+  return (
+    <div className='menu-item'>
+        <div className="content">
+            <h1>Hats</h1>
+            <span>Shop Now</span>
+        </div>
+    </div>
+  )
+}
